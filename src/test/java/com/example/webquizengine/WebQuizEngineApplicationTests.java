157feb7cf8691c057e50f3ff41994b2aa364da3a
Vadim Khizhin <martin.faulther@yandex.ru>
2020-09-04 12:59:42 +0300

    Initial Commit

---
+++ src/test/java/com/example/webquizengine/WebQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webquizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
